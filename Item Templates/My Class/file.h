9a99b34c35da34dd3f9e8f19da3be95c7e21780c
Ilya Seleznev <itsuart@gmail.com>
2019-03-07 21:39:46 +0700

    Add My Class item template

---
+++ Item Templates/My Class/file.h
@@ -0,0 +1,24 @@
+#pragma once
+
+namespace $rootnamespace$ {
+    class $itemname$ final {
+    public:
+        explicit $itemname$();
+        virtual ~$itemname$();
+
+        $itemname$(const $itemname$& other) = delete;
+        $itemname$& operator=(const $itemname$& other) = delete;
+
+        $itemname$($itemname$&& other) = delete;
+        $itemname$& operator=($itemname$&& other) = delete;
+
+        // returns true if state of the object is valid
+        explicit operator bool() const;
+
+        // returns true if state of the object is valid
+        bool ok() const;
+
+    private:
+
+    };
+}
